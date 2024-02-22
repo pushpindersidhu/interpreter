0adfc2af02b427078bd142fc54ff47bbfdb6b274
Pushpinder Sidhu <pushpindersidhu1984@gmail.com>
2024-02-22 01:16:42 -0800

    go: eval boolean comparison

--- go/evaluator/evaluator.go
+++ go/evaluator/evaluator.go
@@ -96,6 +96,10 @@ func evalInfixExpression(
 	switch {
 	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
 		return evalIntegerInfixExpression(operator, left, right)
+    case operator == "==":
+        return nativeBoolToBooleanObject(left == right)
+    case operator == "!=":
+        return nativeBoolToBooleanObject(left != right)
 	default:
 		return nil
 	}
