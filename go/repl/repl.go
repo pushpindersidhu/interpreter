defa765debaab610bdebb00fd57800b67ad7bfd8
Pushpinder Sidhu <pushpindersidhu1984@gmail.com>
2024-02-22 18:34:11 -0800

    go: env and bindings

--- go/repl/repl.go
+++ go/repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"interpreter/evaluator"
 	"interpreter/lexer"
+	"interpreter/object"
 	"interpreter/parser"
 	"io"
 )
@@ -13,6 +14,7 @@ const PROMPT = ">>> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+    env := object.NewEnvironment()
 
 	for {
 		fmt.Printf(PROMPT)
@@ -32,7 +34,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
