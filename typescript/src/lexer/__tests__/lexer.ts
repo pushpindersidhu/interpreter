4aaf1d9437284d5de970d9cb99e269062f8fd910
Pushpinder Sidhu <pushpindersidhu1984@gmail.com>
2024-02-23 15:23:19 -0800

    Remove unused code

--- typescript/src/lexer/__tests__/lexer.ts
+++
@@ -1,7 +0,0 @@
-import { TokenType } from "./lexer.ts";
-
-test("test getNextToken()", function() {
-    input:= `=+(){},;`;
-
-    const tokens = {
-        Token.P:
--- typescript/src/token/token.ts
+++ typescript/src/token/token.ts
@@ -1,69 +1,53 @@
 export const TokenType = {
-    EOF: "EOF",
-    IDENT: "IDENT",
-    ILLEGAL: "ILLEGAL",
-    INT: "INT",
+  EOF: "EOF",
+  IDENT: "IDENT",
+  ILLEGAL: "ILLEGAL",
+  INT: "INT",
 
-    ASSIGN: "=",
-    PLUS: "+",
-    MINUS: "-",
-    BANG: "!",
-    ASTERISK: "*",
-    SLASH: "/",
-    LT: "<",
-    GT: ">",
-    EQUAL: "==",
-    NOTEQUAL: "!=",
+  ASSIGN: "=",
+  PLUS: "+",
+  MINUS: "-",
+  BANG: "!",
+  ASTERISK: "*",
+  SLASH: "/",
+  LT: "<",
+  GT: ">",
+  EQUAL: "==",
+  NOTEQUAL: "!=",
 
-    COMMA: ",",
-    SEMICOLON: ";",
-    LPAREN: "(",
-    RPAREN: ")",
-    LBRACE: "{",
-    RBRACE: "}",
+  COMMA: ",",
+  SEMICOLON: ";",
+  LPAREN: "(",
+  RPAREN: ")",
+  LBRACE: "{",
+  RBRACE: "}",
 
-    LET: "LET",
-    TRUE: "TRUE",
-    FALSE: "FALSE",
-    IF: "IF",
-    ELSE: "ELSE",
-    FUNCTION: "FUNCTION",
-    RETURN: "RETURN",
+  LET: "LET",
+  TRUE: "TRUE",
+  FALSE: "FALSE",
+  IF: "IF",
+  ELSE: "ELSE",
+  FUNCTION: "FUNCTION",
+  RETURN: "RETURN",
 } as const;
 
 export const Keywords = {
-    fn: TokenType.FUNCTION,
-    let: TokenType.LET,
-    true: TokenType.TRUE,
-    false: TokenType.FALSE,
-    if: TokenType.IF,
-    else: TokenType.ELSE,
-    return: TokenType.RETURN,
+  fn: TokenType.FUNCTION,
+  let: TokenType.LET,
+  true: TokenType.TRUE,
+  false: TokenType.FALSE,
+  if: TokenType.IF,
+  else: TokenType.ELSE,
+  return: TokenType.RETURN,
 }
 
 export function lookUpIdent(ident: string): TokenItem {
-    return Keywords[ident as keyof typeof Keywords] || TokenType.IDENT;
+  return Keywords[ident as keyof typeof Keywords] || TokenType.IDENT;
 }
 
 export type TokenItem = (typeof TokenType)[keyof typeof TokenType];
 
 export type Token = {
-    Type: TokenItem;
-    Literal: string;
+  Type: TokenItem;
+  Literal: string;
 };
-
-type Identifier = {
-    Token: Token;
-    Value: string;
-}
-
-type Expression = {
-
-}
-
-type LetStatement = {
-    Token: Token;
-    Name: Identifier;
-    Value: Expression;
-}
-
