3989cbace2d4598c1bd369bd406f20701733c325
Pushpinder Sidhu <pushpindersidhu1984@gmail.com>
2024-02-23 02:29:53 -0800

    go: builtin fn puts()

--- go/evaluator/builtins.go
+++ go/evaluator/builtins.go
@@ -1,6 +1,9 @@
 package evaluator
 
-import "interpreter/object"
+import (
+	"fmt"
+	"interpreter/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": {
@@ -109,6 +112,16 @@ var builtins = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
+		},
+	},
+
+	"puts": {
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
 }
