f3a7568368e1d0509585fe7f82247b05f59e0ae0
Pushpinder Sidhu <pushpindersidhu1984@gmail.com>
2024-02-23 00:58:50 -0800

    go: arrays

--- go/lexer/lexer_test.go
+++ go/lexer/lexer_test.go
@@ -28,6 +28,7 @@ if (5 < 10) {
 let newVar = 5;
 "foobar"
 "foo bar"
+[1, 2];
 `
 	tests := []struct {
 		expectedType    token.TokenType
@@ -113,6 +114,12 @@ let newVar = 5;
         {token.SEMICOLON, ";"},
         {token.STRING, "foobar"},
         {token.STRING, "foo bar"},
+        {token.LBRACKET, "["},
+        {token.INT, "1"},
+        {token.COMMA, ","},
+        {token.INT, "2"},
+        {token.RBRACKET, "]"},
+        {token.SEMICOLON, ";"},
         {token.EOF, ""},
 	}
 
--- go/object/object.go
+++ go/object/object.go
@@ -16,6 +16,7 @@ const (
 	FUNCTION_OBJ     = "FUNCTION"
 	STRING_OBJ       = "STRING"
 	BUILTIN_OBJ      = "BUILTIN"
+	ARRAY_OBJ        = "ARRAY"
 )
 
 type ObjectType string
@@ -97,3 +98,23 @@ type Builtin struct {
 
 func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
 func (b *Builtin) Inspect() string  { return "builtin function" }
+
+type Array struct {
+	Elements []Object
+}
+
+func (ao *Array) Type() ObjectType { return ARRAY_OBJ }
+func (ao *Array) Inspect() string {
+	var out bytes.Buffer
+
+	elements := []string{}
+	for _, el := range ao.Elements {
+		elements = append(elements, el.Inspect())
+	}
+
+	out.WriteString("[")
+	out.WriteString(strings.Join(elements, ", "))
+	out.WriteString("]")
+
+	return out.String()
+}
