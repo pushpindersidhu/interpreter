0f27f52a5b5028280c8d3347eb7e19bafa459f63
Pushpinder Sidhu <pushpindersidhu1984@gmail.com>
2024-02-22 20:00:53 -0800

    go: eval fn & fn calls

--- go/evaluator/evaluator_test.go
+++ go/evaluator/evaluator_test.go
@@ -133,7 +133,8 @@ func TestReturnStatements(t *testing.T) {
             }
             `,
 			10,
-		}}
+		},
+	}
 
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
@@ -218,11 +219,64 @@ func TestLetStatements(t *testing.T) {
 	}
 }
 
+func TestFunctionObject(t *testing.T) {
+	input := "fn(x) { x + 2 };"
+
+	evaluated := testEval(input)
+	fn, ok := evaluated.(*object.Function)
+	if !ok {
+		t.Fatalf("object expected to be *object.Function, got=%T", evaluated)
+	}
+
+	if len(fn.Parameters) != 1 {
+		t.Fatalf("parameter length wrong. expected=1, got=%d",
+			len(fn.Parameters))
+	}
+
+	if fn.Parameters[0].String() != "x" {
+		t.Fatalf("parameter expected='x', got=%q", fn.Parameters[0])
+	}
+
+	expectedBody := "(x + 2)"
+	if fn.Body.String() != expectedBody {
+		t.Fatalf("body expected=%q. got=%q", expectedBody, fn.Body.String())
+	}
+}
+
+func TestFunctionApplication(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"let identity = fn(x) { x; }; identity(5);", 5},
+		{"let identity = fn(x) { return x; }; identity(5);", 5},
+		{"let double = fn(x) { x * 2; }; double(5);", 10},
+		{"let add = fn(x, y) { x + y; }; add(5, 5);", 10},
+		{"let add = fn(x, y) { x + y; }; add(5 + 5, add(5, 5));", 20},
+		{"fn(x) { x; }(5)", 5},
+	}
+
+	for _, tt := range tests {
+		testIntegerObject(t, testEval(tt.input), tt.expected)
+	}
+}
+
+func TestClosures(t *testing.T) {
+	input := `
+    let newAdder = fn(x) {
+        fn(y) { x + y };
+    };
+    let addTwo = newAdder(2);
+    addTwo(2);`
+
+	testIntegerObject(t, testEval(input), 4)
+}
+
 func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
 	program := p.ParseProgram()
-    env := object.NewEnvironment()
+	env := object.NewEnvironment()
 
 	return Eval(program, env)
 }
--- go/object/environment.go
+++ go/object/environment.go
@@ -2,6 +2,7 @@ package object
 
 type Environment struct {
 	store map[string]Object
+    outer *Environment
 }
 
 func NewEnvironment() *Environment {
@@ -9,8 +10,19 @@ func NewEnvironment() *Environment {
 	return &Environment{store: s}
 }
 
+func NewEnclosedEnvrionment(outer *Environment) *Environment {
+    env := NewEnvironment()
+    env.outer = outer
+
+    return env
+}
+
 func (e *Environment) Get(name string) (Object, bool) {
 	obj, ok := e.store[name]
+    if !ok && e.outer != nil {
+        obj, ok = e.outer.Get(name)
+    }
+
 	return obj, ok
 }
 
@@ -18,3 +30,4 @@ func (e *Environment) Set(name string, val Object) Object {
 	e.store[name] = val
 	return val
 }
+
