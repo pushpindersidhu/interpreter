8a76c60f23781828d28b0907506295f665af9ab9
Pushpinder Sidhu <pushpindersidhu1984@gmail.com>
2024-02-26 02:10:01 -0800

    ts: fix env

--- typescript/src/repl/repl.ts
+++ typescript/src/repl/repl.ts
@@ -4,21 +4,21 @@ import { Parser } from "../parser";
 import { Evaluator } from "../evaluator";
 import { Environment } from "../object";
 
+const PROMPT = ">>> ";
+const env = new Environment();
+
+const rs = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: PROMPT,
+});
+
 export function repl() {
-    const PROMPT = ">>> ";
-
-    const rs = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-        prompt: PROMPT,
-    });
-
     rs.on("line", function (input: string) {
         const lexer = new Lexer(input);
         const parser = new Parser(lexer);
         const program = parser.parseProgram();
         const evaluator = new Evaluator();
-        const env = new Environment();
 
         const evaluated = evaluator.eval(program, env);
         console.log(evaluated.inspect());
